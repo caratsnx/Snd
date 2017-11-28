d6bcfa411df2365dc156fb61ff370b3dcd7f7e7f
kushagra <caratsnx@gmail.com>
2017-11-28 20:49:18 +0530

    Initial

---
+++ app/src/main/java/com/poc/tcs/snd/CustomPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.poc.tcs.snd;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+class CustomPagerAdapter extends PagerAdapter{
+
+    Context mContext;
+    LayoutInflater mLayoutInflater;
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return Util.mAbstractCategory.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.page_item, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(Util.mAbstractCategory[position]);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
---
+++ app/src/main/java/com/poc/tcs/snd/Util.java
@@ -0,0 +1,15 @@
+package com.poc.tcs.snd;
+
+/**
+ * Created by kushagra on 28/11/17.
+ */
+ class Util {
+    public final static int[]
+            mAbstractCategory = {
+            R.drawable.b1,
+            R.drawable.b2,
+            R.drawable.b3,
+            R.drawable.b4
+    };
+
+    }
